1a85d22767d5c6c2bcddbf2e35233a7d6ea5c2dd
Arzan Khan <arzankhan889@gmail.com>
2024-02-29 21:54:22 +0530

    added mobile changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -76,7 +76,23 @@ const Navbar =  async () => {
               </div>
             ))}
           </div>
-          <SearchComponent />
+          <div className={styles.mobSearchWrap}>
+            <div className={styles.mobSearchLink}>
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+            </div>
+            <div className={styles.mobSearchContent}>
+              <SearchComponent />
+            </div>
+          </div>
         </div>
         </div>
       </div>
