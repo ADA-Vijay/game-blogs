c51899d484b5d83bd4dc2afc7de01c5e8cb949e2
Farhank19 <farhankazi887@gmail.com>
2024-03-01 19:36:42 +0530

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
+async function getData() {
+    const res = await fetch("https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/categories", {
+    });
+    
+    const rawData = await res.json();
+  // We generate the XML sitemap with the posts data
+  const paths = [];
+  rawData.forEach((post) => {
+      paths.push(`/${post.slug}`);
+    });
+  
+  return  paths ; 
+}
+
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.gamewitted.com/',
-    generateRobotsTxt: false, // (optional)
-    // ...other options
+    generateRobotsTxt: false,
+    additionalPaths : getData()
+    
   }
