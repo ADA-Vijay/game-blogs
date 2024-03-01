cce65dd3cacd7893fd020c11905476904b2dc7a9
Farhank19 <farhankazi887@gmail.com>
2024-03-01 19:48:14 +0530

    sitemap hardcode

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,20 +2,21 @@
 async function getData() {
     const res = await fetch("https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/categories", {
     });
-    
+
     const rawData = await res.json();
-  // We generate the XML sitemap with the posts data
-  const paths = [];
-  rawData.forEach((post) => {
-      paths.push(`/${post.slug}`);
+    // We generate the XML sitemap with the posts data
+    const paths = [];
+    rawData.forEach((post) => {
+        paths.push(`/${post.slug}`);
     });
-  
-  return  paths ; 
+    console.log(paths)
+    return paths;
 }
 
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.gamewitted.com/',
     generateRobotsTxt: false,
-    additionalPaths : getData()
-    
-  }
+    // additionalPaths: await getData()
+    additionalPaths: ["/anime","/gaming","/fortnite-gaming","/genshin-impact","/guides","/helldivers-2","/news","/wwe-2k24"]
+
+}
