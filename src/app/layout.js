0b039e087644ba327764a5d1ae586722c0c59d5e
vijay4527 <vijaysaroj4527@gmail.com>
2024-03-01 12:31:11 +0530

    new changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,38 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
-import { Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "GameWitted",
-     description: "Welcome to AshGamewitted, your ultimate destination for immersive gaming and captivating anime content! Dive into a world where pixels meet passion, as we bring you the latest updates, reviews, and insights from the gaming and anime realms.",
-     images: [
-       {
-         url: "https://fama.b-cdn.net/gw/gwlogo.png",
-         height: 1200,
-         width: 600,
-         alt: "Alt",
-       },
-     ],
+  title: "GameWitted",
+  description:
+    "Welcome to AshGamewitted, your ultimate destination for immersive gaming and captivating anime content! Dive into a world where pixels meet passion, as we bring you the latest updates, reviews, and insights from the gaming and anime realms.",
+  images: [
+    {
+      url: "https://fama.b-cdn.net/gw/gwlogo.png",
+      height: 1200,
+      width: 600,
+      alt: "Alt",
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <meta
+        name="google-site-verification"
+        content="RdxkhXckn9g6Sc2nHYDSF5_Y2fQ3VwWBpEq4ZS_Z2bg"
+      />
 
-            <div className="container">
-              <Navbar />
-              {children}
-              <Footer />
-            </div>
+      <body className={inter.className}>
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
