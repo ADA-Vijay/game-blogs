fc0da8c09fa7eb95cae98ecccfbf905a0ec3264c
vijay4527 <vijaysaroj4527@gmail.com>
2024-03-02 16:34:35 +0530

    breadcrumb added

--- src/components/breadCrumb/breadCrumb.jsx
+++ src/components/breadCrumb/breadCrumb.jsx
@@ -1,24 +1,23 @@
 "use client";
 import React from "react";
 import styles from "@/app/page.module.css";
-import Container from "react-bootstrap/Container";
 import Link from "next/link";
-import NotFound from "@/components/notFound/notFound";
-import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
 const breadCrumb = ({ category, subcategory }) => {
-  const seperator = ">";
   return (
     <div className={styles.latestWrap}>
       <div className={styles.container}>
         <div className={styles.latestBody}>
           <div className={styles.latestContent}>
-            <div className={styles.titleName}>{}</div>
             <div className={styles.latestBox}>
-              <Breadcrumbs className={styles.breadCrumb}>
-                <BreadcrumbItem>{category}</BreadcrumbItem>
-                {seperator}
-                {subcategory ? <BreadcrumbItem>{subcategory}</BreadcrumbItem> : ""}
-              </Breadcrumbs>
+              <div className={styles.breadCrumb}>
+                <Link href={category}>{category}</Link>
+                {subcategory ? 
+                <span> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 12 18" className="text-neutral-grey-70"><path d="m.999 15.613 1.768 1.768 8.383-8.384L2.767.613.999 2.381l6.616 6.616-6.616 6.616Z"></path></svg>
+                    {subcategory}
+                </span> : 
+                ""}
+              </div>
             </div>
           </div>
         </div>
