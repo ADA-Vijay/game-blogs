bbf655ecf3ecb1b76def9cdc4eb01f3337936bb1
vijay4527 <vijaysaroj4527@gmail.com>
2024-03-02 11:08:29 +0530

    new changes

--- src/app/[category]/page.jsx
+++ src/app/[category]/page.jsx
@@ -3,50 +3,46 @@ import styles from "@/app/page.module.css";
 import { notFound } from "next/navigation";
 import HeroBanner from "@/components/heroBanner/heroBanner";
 import { Container } from "react-bootstrap";
-import ListingPage from "@/components/listing/listing"
+import ListingPage from "@/components/listing/listing";
 async function getData(category) {
-    const ApiUrl = "https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/";
-    const categoryResponse = await fetch(
-        `${ApiUrl}categories?slug=${category}`
-      );
+  const ApiUrl = "https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/";
+  const categoryResponse = await fetch(`${ApiUrl}categories?slug=${category}`);
 
-      const catgoryData = await categoryResponse.json();
-      if (!catgoryData || catgoryData.length === 0) {
-        return null;
-      }
-      const categoryId = catgoryData[0].id;
-       let initialData = [];
-       if (categoryId) {
-         const response = await fetch(
-           `${ApiUrl}posts?categories=${categoryId}&per_page=10&_embed`,
-           {
-            next: {revalidate:30},
-          }
-         );
-         const initialData = await response.json()
-         return initialData && initialData.length > 0 ? initialData : null;
-
-       }
-  
+  const catgoryData = await categoryResponse.json();
+  if (!catgoryData || catgoryData.length === 0) {
+    return null;
   }
+  const categoryId = catgoryData[0].id;
+  let initialData = [];
+  if (categoryId) {
+    const response = await fetch(
+      `${ApiUrl}posts?categories=${categoryId}&per_page=10&_embed`,
+      {
+        next: { revalidate: 30 },
+      }
+    );
+    const initialData = await response.json();
+    return initialData && initialData.length > 0 ? initialData : null;
+  }
+}
 
-
-  export async function generateMetadata({ params }) {
-
-  const data = await getData(params.category)
-  if(data && data.length > 0){
+export async function generateMetadata({ params }) {
+  const data = await getData(params.category);
+  if (data && data.length > 0) {
     return {
       title: data[0].yoast_head_json.title,
-       description: data[0].yoast_head_json.description,
-       images: [
-         {
-           url: data[0].yoast_head_json.og_image[0].url,
-           height: 1200,
-           width: 600,
-           alt: "Alt",
-         },
-       ],
-   };
+      description: data[0].yoast_head_json.description,
+      openGraph: {
+        images: [
+          {
+            url: data[0].yoast_head_json.og_image[0].url,
+            height: 1200,
+            width: 600,
+            alt: "Alt",
+          },
+        ],
+      },
+    };
   }
   // else{
   //   return {
@@ -62,27 +58,24 @@ async function getData(category) {
   //      ],
   //  };
   // }
-
 }
 
-const Page = async({ params }) => {
-
-    const category = params.category
-    const data = await  getData(category)
- if(!data){
-    return notFound()
+const Page = async ({ params }) => {
+  const category = params.category;
+  const data = await getData(category);
+  if (!data) {
+    return notFound();
   }
   return (
     <div>
-     <div className={styles.latestWrap}>
-      <Container>
+      <div className={styles.latestWrap}>
+        <Container>
           <HeroBanner></HeroBanner>
-           </Container>
-    </div> 
-    <ListingPage newdata={data} />
-
-  </div>
+        </Container>
+      </div>
+      <ListingPage newdata={data} />
+    </div>
   );
-}
+};
 
 export default Page;
