68fcb73c9455be3d5af98f11f4bde679fe3f5118
vijay4527 <vijaysaroj4527@gmail.com>
2024-03-02 17:21:23 +0530

    breadcrumb changes

--- src/components/breadCrumb/breadCrumb.jsx
+++ src/components/breadCrumb/breadCrumb.jsx
@@ -10,7 +10,7 @@ const breadCrumb = ({ category, subcategory }) => {
           <div className={styles.latestContent}>
             <div className={styles.latestBox}>
               <div className={styles.breadCrumb}>
-                <Link href={category}>{category}</Link>
+                <Link href={`/${category}`}>{category}</Link>
                 {subcategory ? 
                 <h4> 
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 12 18" className="text-neutral-grey-70"><path d="m.999 15.613 1.768 1.768 8.383-8.384L2.767.613.999 2.381l6.616 6.616-6.616 6.616Z"></path></svg>
