04dcf874238f2f465942ab9940969a0406cc8b2e
Farhank19 <farhankazi887@gmail.com>
2024-03-14 15:41:53 +0530

    fav test2

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,13 +43,14 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="RdxkhXckn9g6Sc2nHYDSF5_Y2fQ3VwWBpEq4ZS_Z2bg"
         />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="theme-color" content="#ffffff" />
         {/* <link
           href={"/favImage/apple-touch-icon.png"}
           rel="apple-touch-icon"
