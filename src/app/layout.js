2c37cdbd98e0991a8e45261686d96eb10080d840
Farhank19 <farhankazi887@gmail.com>
2024-03-14 15:00:42 +0530

    fav icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,7 +44,11 @@ export default function RootLayout({ children }) {
           content="RdxkhXckn9g6Sc2nHYDSF5_Y2fQ3VwWBpEq4ZS_Z2bg"
         />
         <link href={"/favicon.ico"} rel="icon" sizes="any" />
-        <link
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        {/* <link
           href={"/favImage/apple-touch-icon.png"}
           rel="apple-touch-icon"
           sizes="180x180"
@@ -60,7 +64,7 @@ export default function RootLayout({ children }) {
           rel="icon"
           sizes="16x16"
           type="image/png"
-        />
+        /> */}
       </Head>
 
       <body className={inter.className}>
