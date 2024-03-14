715a0f5a164af3545a40f9217d58fdd6cf30d893
vijay4527 <vijaysaroj4527@gmail.com>
2024-03-14 16:56:25 +0530

    new changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,23 +53,6 @@ export default function RootLayout({ children }) {
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
-        {/* <link
-          href={"/favImage/apple-touch-icon.png"}
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href={"/favImage/favicon/32x32.png"}
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href={"/favImage/favicon-16x16.png"}
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        /> */}
       </Head>
 
       <body className={inter.className}>
