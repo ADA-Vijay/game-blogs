7fbaf90f04a10c195d33e4a0a313c89a4e29a4e7
vijay4527 <vijaysaroj4527@gmail.com>
2024-03-28 13:03:40 +0530

    new changes

--- src/app/post/sitemap.js
+++ src/app/post/sitemap.js
@@ -2,15 +2,11 @@ export default async function sitemap() {
     try {
     
         const paths = ['/'];
-        const posts = await fetch("https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/posts?&_embed", {
-            next: { revalidate: 180 },
-          });
-        if (!posts.ok) {
-            throw new Error(`Failed to fetch data: ${posts.statusText}`);
-        }
-        const postsData = await posts.json();
+        const posts = await fetchAllPosts("https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/posts?&_embed"
+           );
+        
 
-        postsData.forEach((post) => {
+           posts.forEach((post) => {
             paths.push(`/${post._embedded["wp:term"][0][0].slug}/${post.slug}`);
         });
 
@@ -36,5 +32,50 @@ export default async function sitemap() {
 }
 
 
+async function fetchAllPosts(url, posts = []) {
+    try {
+        const response = await fetch(url,
+            {
+                next: { revalidate: 180 },
+              }
+            );
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+        const newposts = await response.json();
+        if (newposts.length === 0) {
+            return posts;
+        }
+        
+        posts = posts.concat(newposts);
+
+        const nextPageUrl = getNextPageUrl(response.headers.get('link'));
+        
+        if (nextPageUrl) {
+            return fetchAllPosts(nextPageUrl, posts);
+        } else {
+            return posts;
+        }
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+        return [];
+    }
+}
+
+function getNextPageUrl(linkHeader) {
+    if (!linkHeader) {
+        return null;
+    }
+    
+    const links = linkHeader.split(',');
+    for (const link of links) {
+        const match = link.match(/<([^>]+)>;\s*rel="next"/);
+        if (match) {
+            return match[1];
+        }
+    }
+    
+    return null;
+}
 
 
