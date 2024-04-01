db18461ce778e266226f5aa86688ea8e1572d730
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-01 11:58:44 +0530

    new changes

--- src/app/post/sitemap.js
+++ src/app/post/sitemap.js
@@ -1,81 +1,75 @@
 export default async function sitemap() {
-    try {
-    
-        const paths = ['/'];
-        const posts = await fetchAllPosts("https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/posts?&_embed"
-           );
-        
+  try {
+    const paths = ["/"];
+    const posts = await fetchAllPosts(
+      "https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/posts?&_embed"
+    );
 
-           posts.forEach((post) => {
-            paths.push(`/${post._embedded["wp:term"][0][0].slug}/${post.slug}`);
-        });
+    posts.forEach((post) => {
+      paths.push(`/${post._embedded["wp:term"][0][0].slug}/${post.slug}`);
+    });
 
-        return [
-            ...paths.map((path) => ({
-                url: `https://www.gamewitted.com${path}`,
-                lastModified: new Date(),
-                changeFrequency: 'daily',
-                priority: 1,
-            })),
-        ];
-    } catch (error) {
-        console.error("Error generating sitemap:", error.message);
-        return [
-            {
-                url: `https://www.gamewitted.com`,
-                lastModified: new Date(),
-                changeFrequency: 'daily',
-                priority: 1,
-            }
-        ];
-    }
+    return [
+      ...paths.map((path) => ({
+        url: `https://www.gamewitted.com${path}`,
+        lastModified: new Date(),
+        changeFrequency: "daily",
+        priority: 1,
+      })),
+    ];
+  } catch (error) {
+    console.error("Error generating sitemap:", error.message);
+    return [
+      {
+        url: `https://www.gamewitted.com`,
+        lastModified: new Date(),
+        changeFrequency: "daily",
+        priority: 1,
+      },
+    ];
+  }
 }
 
-
 export async function fetchAllPosts(url, posts = []) {
-    try {
-        const response = await fetch(url,
-            {
-                cache: 'no-store'
-              }
-            );
-        if (!response.ok) {
-            throw new Error(`Failed to fetch data: ${response.statusText}`);
-        }
-        const newposts = await response.json();
-        if (newposts.length === 0) {
-            return posts;
-        }
-        
-        posts = posts.concat(newposts);
-
-        const nextPageUrl = getNextPageUrl(response.headers.get('link'));
-        
-        if (nextPageUrl) {
-            return fetchAllPosts(nextPageUrl, posts);
-        } else {
-            return posts;
-        }
-    } catch (error) {
-        console.error('Error fetching categories:', error);
-        return [];
+  try {
+    const response = await fetch(url, {
+        cache: 'no-store'
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
     }
+    const newposts = await response.json();
+    if (newposts.length === 0) {
+      return posts;
+    }
+
+    posts = posts.concat(newposts);
+
+    const nextPageUrl = getNextPageUrl(response.headers.get("link"));
+
+    if (nextPageUrl) {
+      return fetchAllPosts(nextPageUrl, posts);
+    } else {
+      return posts;
+    }
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
 }
 
 function getNextPageUrl(linkHeader) {
-    if (!linkHeader) {
-        return null;
-    }
-    
-    const links = linkHeader.split(',');
-    for (const link of links) {
-        const match = link.match(/<([^>]+)>;\s*rel="next"/);
-        if (match) {
-            return match[1];
-        }
-    }
-    
+  if (!linkHeader) {
     return null;
+  }
+
+  const links = linkHeader.split(",");
+  for (const link of links) {
+    const match = link.match(/<([^>]+)>;\s*rel="next"/);
+    if (match) {
+      return match[1];
+    }
+  }
+
+  return null;
 }
-
-
