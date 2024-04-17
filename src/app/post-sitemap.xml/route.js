9e29defe0e14ee945ad83306c298e2a753c34e44
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-17 15:54:29 +0530

    post-sitemap changes

--- src/app/post-sitemap.xml/route.js
+++ src/app/post-sitemap.xml/route.js
@@ -1,7 +1,7 @@
 const itemsPerPage = 100
 export async function GET(req) {
-  // const page = new URL(req.url).searchParams.get("page")
-  // if(page == null) return new Response();
+  const page = new URL(req.url).searchParams.get("page")
+  if(page == null) return new Response();
   const url = await getPost(page);
   return new Response(`<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd">
   ${url || ""}
