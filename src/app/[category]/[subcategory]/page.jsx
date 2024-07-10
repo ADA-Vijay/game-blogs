e8604e8ff18c7b0d74abf35f0c31051c3d9a3989
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-10 11:32:26 +0530

    twitter tag added

--- src/app/[category]/[subcategory]/page.jsx
+++ src/app/[category]/[subcategory]/page.jsx
@@ -122,6 +122,12 @@ const page = async ({ params }) => {
     <>
       {data && data.length > 0 && (
         <>
+          <Head>
+            <meta
+              property="twitter:image:src"
+              content={data[0].yoast_head_json.og_image[0].url}
+            />
+          </Head>
           <div className={styles.latestWrap}>
             <div className={styles.container}>
               <BreadCrumb category={category} subcategory={subcategory} />
