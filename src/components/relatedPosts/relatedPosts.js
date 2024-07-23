6693518c5edde13d7b6544f7f3fbcffa4b9e3830
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-23 13:32:36 +0530

    post by category changes

--- src/components/relatedPosts/relatedPosts.js
+++ src/components/relatedPosts/relatedPosts.js
@@ -1,39 +1,45 @@
-import React from 'react'
+import React from "react";
 import styles from "@/app/page.module.css";
-import Link from 'next/link';
-import style from './relatedPosts.module.css'
+import Link from "next/link";
+import style from "./relatedPosts.module.css";
 
-const relatedPosts = ({ data }) => {
-    const formatDate = (isoDate) => {
-        const options = { day: "2-digit", month: "long", year: "numeric" };
-        const date = new Date(isoDate);
-        return date.toLocaleDateString("en-US", options);
-    };
-    console.log("data",data)
-    return (
-        <>
-
-            <div className={style["container"]}>
-                <div className={style["container-wrap"]}>
-                    <h1 className={style["relatedpost-title"]}>Related Post</h1>
-                    <div className={style["post-card-wrap"]}>
-                        {data && data.length > 0 && data.slice(0, 4).map((e) => (
-                            <div className={style["post-card-item"]}>
-                                <div className={style["post-img-div"]}>
-                                    <img src={e.jetpack_featured_media_url}></img>
-                                </div>
-                                <div>
-                                    {/* <p className={style["green-p"]}>MMA</p> */}
-                                    <p className={style["desc-p"]}>{e.yoast_head_json.description}</p>
-                                    <p className={style["name-p"]}>{e.yoast_head_json.author}</p>
-                                </div>
-                            </div>
-                        ))}
+const relatedPosts = ({ category,data }) => {
+  const formatDate = (isoDate) => {
+    const options = { day: "2-digit", month: "long", year: "numeric" };
+    const date = new Date(isoDate);
+    return date.toLocaleDateString("en-US", options);
+  };
+  return (
+    <>
+      <div className={style["container"]}>
+        <div className={style["container-wrap"]}>
+          <h1 className={style["relatedpost-title"]}>Related Post</h1>
+          <div className={style["post-card-wrap"]}>
+            {data &&
+              data.length > 0 &&
+              data.slice(0, 4).map((e,i) => (
+                <Link href={`/${category}/${e.slug}`} key={i}>
+                  <div className={style["post-card-item"]}>
+                    <div className={style["post-img-div"]}>
+                      <img src={e.jetpack_featured_media_url}></img>
                     </div>
-                </div>
-            </div>
-        </>
-    )
-}
+                    <div>
+                      {/* <p className={style["green-p"]}>MMA</p> */}
+                      <p className={style["desc-p"]}>
+                        {e.yoast_head_json.title}
+                      </p>
+                      <p className={style["name-p"]}>
+                        {e.yoast_head_json.author}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default relatedPosts
+export default relatedPosts;
