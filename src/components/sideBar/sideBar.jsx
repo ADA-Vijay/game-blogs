a02eac0ab6b5133f4f8c7336dfe055c250e6f06d
ADA-AbdusSamad <ada.abdussamad@gmail.com>
2024-07-25 13:25:01 +0530

    sidebar data fetch

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -4,45 +4,66 @@ import styles from "./sideBar.module.css";
 import { useState } from "react";
 import Link from "next/link";
 function sideBar({ data }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const handleToggle = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <div>
-      <div className={styles.hamBurgerButton}>
-        <div className={styles.siderBarIcon} onClick={handleToggle}>
-          <i className="fa-solid fa-bars"></i>
+    const handleToggle = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+    console.log(data)
+
+    return (
+        <div>
+            <div className={styles.hamBurgerButton}>
+                <div className={styles.siderBarIcon} onClick={handleToggle}>
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+                {sidebarOpen && (
+                    <div
+                        className={`${styles.sidebar} ${sidebarOpen ? styles.sidebarOpen : ""
+                            }`}
+                    >
+                        <div className={styles.siderbarContainer}>
+                            <div onClick={handleToggle} className={styles.sideBarCloseIcon}>
+                                <i className="fa-solid fa-xmark"></i>
+                            </div>
+                        </div>
+                        <div className={styles["content-container"]}>
+                            <ul className={styles["data-content-ul"]}>
+                                {data &&
+                                    data.length > 0 &&
+                                    data.map((e) => (
+                                        <li className={styles["data-content-li"]} key={e.id}>
+                                            <div onClick={toggleAccordion} className={styles.acordianDiv}>
+                                                <Link href={e.slug}>{e.name}</Link>
+                                                <div className={`${styles.acordianIcon} ${isOpen ? styles.rotate : styles.rotateNormal}`}>
+                                                    <i class="fa-solid plus fa-plus"></i>
+                                                </div>
+                                            </div>
+                                            <div className={`${styles.liData} ${isOpen ? styles.open : ''}`}>
+                                                <ul>
+                                                    {
+                                                        e.children && e.children.length > 0 && e.children.map((e) => (
+                                                            <li key={e.id}>
+                                                                {e.name}
+                                                            </li>
+                                                        ))
+                                                    }
+                                                </ul>
+                                            </div>
+                                        </li>
+                                    ))}
+                            </ul>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-        {sidebarOpen && (
-          <div
-            className={`${styles.sidebar} ${
-              sidebarOpen ? styles.sidebarOpen : ""
-            }`}
-          >
-            <div className={styles.siderbarContainer}>
-              <div onClick={handleToggle} className={styles.sideBarCloseIcon}>
-                <i className="fa-solid fa-xmark"></i>
-              </div>
-            </div>
-            <div className={styles["content-container"]}>
-              <ul>
-                {data &&
-                  data.length > 0 &&
-                  data.map((e) => (
-                    <li key={e.id}>
-                      <Link href={e.slug}>{e.name}</Link>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default sideBar;
