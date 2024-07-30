a02193131beb1190dcbbbef0b63281859537c005
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-30 18:13:00 +0530

    title changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta property="og:site_name" content="GameWitted" />
+        <meta property="og:title" content="GameWitted" />
         <meta
           property="og:description"
           content="Welcome to Gamewitted! Dive into immersive gaming and anime content with the latest updates, reviews, and insights. Where pixels meet passion!"
