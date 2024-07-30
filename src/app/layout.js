f1fe31bbe81f42f058365657aba6fc8f1bc47501
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-30 18:30:37 +0530

    new changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import Script from "next/script";
-import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
