c0e1d86cadf12a76117fcd5ab942cc8ffaaf0178
vijay4527 <vijaysaroj4527@gmail.com>
2024-08-10 20:23:52 +0530

    new changes

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -54,6 +54,119 @@ const page = () => {
             find elsewhere.
           </li>
         </ul>
+
+        <h1>Our Team</h1>
+        <span className={styles.ourTeam}>
+          At the heart of GameWitted is our diverse team of four industry
+          veterans, each bringing unique expertise and perspectives to our
+          content:
+        </span>
+        <ul style={{ marginTop: "10px" }}>
+          <li className={styles.list}>
+            Former writers from renowned platforms like Dexerto, PCInvasion, and
+            more
+          </li>
+          <li className={styles.list}>
+            Esports athletes for MOBAs like Pokemon Unite, Wild Rift, Arena of
+            Valor, and more.
+          </li>
+          <li className={styles.list}>Experienced esports analysts.</li>
+          <li className={styles.list}>
+            Passionate gamers with deep knowledge across multiple genres and
+            platforms .
+          </li>
+          <li className={styles.list}>
+            Anime aficionados with a keen eye for storytelling and cultural
+            nuances
+          </li>
+        </ul>
+        <span className={styles.ourTeam}>
+          This mix of skills helps us understand games from many angles, giving
+          you insights you won't find elsewhere.
+        </span>
+        <span className={styles.ourTeam}>
+          At GameWitted, every article is a labor of love. Whether we're
+          explaining complex strategies for a AAA title, interviewing a
+          developer, or analyzing the latest esports match, we put our heart
+          into every word.
+        </span>
+        <span className={styles.ourTeam}>
+          We're here to celebrate games in all their forms, start interesting
+          conversations, and grow alongside our community. GameWitted isn't just
+          a website – it's a gathering place for people who love games as much
+          as we do.
+        </span>
+
+        <h1>What We Offer</h1>
+        <span className={styles.ourTeam}>
+          At GameWitted, we pride ourselves on our comprehensive coverage of the
+          gaming and anime worlds:
+        </span>
+        <ul style={{ marginTop: "10px" }}>
+          <li className={styles.list}>
+            Gaming News and Reviews: Stay up-to-date with the latest releases,
+            updates, and industry trends.
+          </li>
+          <li className={styles.list}>
+            Spotlight on Underdogs: While we love big games, we're not afraid to
+            champion lesser-known titles that deserve attention.
+          </li>
+          <li className={styles.list}>
+            In-Depth Guides: Master your favorite games with our expertly
+            crafted, step-by-step guides.
+          </li>
+          <li className={styles.list}>
+            Esports Coverage: From match analyses to player interviews, we bring
+            you inside the competitive gaming scene.
+          </li>
+          <li className={styles.list}>
+            Anime Insights: Dive deep into chapter analyses, character studies,
+            and thematic explorations of popular anime series.
+          </li>
+          <li className={styles.list}>
+            Exclusive Interviews: Get to know the minds behind your favorite
+            games through our conversations with developers, designers, and
+            industry leaders.
+          </li>
+          <li className={styles.list}>
+            Community Focus: Your voice matters to us. We want to hear what you
+            think and include your ideas in our discussions. So please mail us
+            whenever you want and whatever you want. We will always reply!
+          </li>
+        </ul>
+
+        <h1>Our Approach</h1>
+        <ul style={{ marginTop: "10px" }}>
+          <li className={styles.list}>
+            Journalistic Integrity: We prioritize accuracy, fairness, and
+            transparency in all our reporting.
+          </li>
+          <li className={styles.list}>
+            Spotlight on Underdogs: While we love big games, we're not afraid to
+            champion lesser-known titles that deserve attention.
+          </li>
+          <li className={styles.list}>
+            Diversity and Inclusion: We celebrate the rich diversity of the
+            gaming and anime communities, ensuring representation in our content
+            and team.
+          </li>
+          <li className={styles.list}>
+            Ethical Standards: We maintain strict guidelines against promoting
+            gambling or any content that could be harmful to our community
+          </li>
+        </ul>
+
+        <h1>Looking Ahead</h1>
+        <span className={styles.description}>
+          As we continue to grow, GameWitted remains committed to evolving with
+          the dynamic worlds of gaming and anime. We're constantly exploring new
+          content formats, technologies, and partnerships to enhance your
+          experience and keep you at the forefront of gaming culture.
+        </span>
+        <span className={styles.description}>
+        Join us as we continue to shape the landscape of gaming and anime content creation.
+        GameWitted: Where Gaming Wisdom Meets Passion, and Every Player Has a Voice.
+        </span>
       </div>
     </>
   );
